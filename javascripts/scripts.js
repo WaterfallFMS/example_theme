d787b48115eec2e097d98c30b59151a64dd604d8
John Kamenik <jkamenik@gmail.com>
2013-03-18 15:30:56 -0400

    Replacing SAME Javascript

---
+++ javascripts/scripts.js
@@ -0,0 +1,26 @@
+// Begin jQuery Stuff
+jQuery.noConflict();
+
+jQuery(document).ready(function($) { 
+
+  $('h1,h2,h3,h4,h5,h6').each(function(){
+    console.log($(this).hasClass('skip'));
+    if($(this).hasClass('skip')){ return; }
+    
+    var values = $(this).text().split(' ');
+    var first = values.shift();
+    var rest = values.join(' ');
+    $(this).html("<span class='first'>"+first+"</span><span class='second'>"+rest+"</span>");
+  });
+
+
+  // Superfish
+  jQuery('#nav ul').superfish({
+    hoverClass: 'sfhover',
+    delay: 400,
+    animation: {opacity:'show'},
+    autoArrows: false,
+    speed: 'fast'
+  }); 
+      
+}); 
