800af6a0b2e91db9c21b34990f12e4c389826bf7
John Kamenik <jkamenik@gmail.com>
2013-03-28 15:24:41 -0400

    Using jQuery to capitalize the first word

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -4,13 +4,19 @@ jQuery.noConflict();
 jQuery(document).ready(function($) { 
 
   $('h1,h2,h3.with-style').each(function(){
-    console.log($(this).hasClass('skip'));
     if($(this).hasClass('skip')){ return; }
     
     var values = $(this).text().split(' ');
     var first = values.shift();
     var rest = values.join(' ');
     $(this).html("<span class='first'>"+first+"</span><span class='second'>"+rest+"</span>");
+  });
+  
+  $('.first_story .story_title a').each(function(){
+    var values = $(this).text().split(' ');
+    var first = values.shift();
+    var rest  = values.join(' ');
+    $(this).html("<span class='uppercase'>"+first+"</span> "+rest);
   });
 
 
