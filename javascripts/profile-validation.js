f0092eac2c323558252a57f4f7abab89d0443435
Zach Rowe <zach@alum.rpi.edu>
2013-10-31 10:44:13 -0400

    add comment, don't specify the protocol

--- javascripts/profile-validation.js
+++ javascripts/profile-validation.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
     $(".edit_directory_company").validate({
         rules: {
+        /* use the email rule as a hook to trim the field of
+         * whitespace. The test returns true, so the field
+         * isn't really required
+         */
             email: {
                 required: {
                     depends:function(){
